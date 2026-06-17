485d4c93ef5ba3d039d49d71ccdd6e03fafa1778
savio-kenneth-myers <savio.encrypted@gmail.com>
2026-06-17 20:08:45 +0530

    08_ride_length_by_days.sql

--- SQL/08_weekend_ride_length.sql
+++ SQL/08_weekend_ride_length.sql
@@ -1,11 +1,19 @@
--- Weekend Rides: average ride length on Saturdays by rider type
+-- Weekend Rides: average ride length on days by rider type
 -- Reveals casual riders take their longest rides on Saturdays (18.2 min avg)
 
 SELECT
-    day_of_week,
-    member_casual,
-    ROUND(AVG(ride_length_mins), 1) AS avg_ride_length_mins
-FROM trips
-WHERE day_of_week IN ('Saturday', 'Sunday')
-GROUP BY day_of_week, member_casual
-ORDER BY day_of_week, member_casual;
+        day_of_week,
+        member_casual                                   AS rider_type,
+        ROUND(AVG(ride_length_mins), 2)                 AS avg_ride_mins
+    FROM trips
+    GROUP BY day_of_week, member_casual
+    ORDER BY
+        CASE day_of_week
+            WHEN 'Monday'    THEN 1
+            WHEN 'Tuesday'   THEN 2
+            WHEN 'Wednesday' THEN 3
+            WHEN 'Thursday'  THEN 4
+            WHEN 'Friday'    THEN 5
+            WHEN 'Saturday'  THEN 6
+            WHEN 'Sunday'    THEN 7
+        END
