66340673c2da5cd85b26ea14c5dcbacf4bda3f05
savio-kenneth-myers <savio.encrypted@gmail.com>
2026-06-17 20:03:06 +0530

    03_rides_by_day.sql

--- SQL/03_rides_by_day.sql
+++ SQL/03_rides_by_day.sql
@@ -2,19 +2,21 @@
 -- Reveals members peak Tuesday, casuals peak Saturday
 
 SELECT
-    day_of_week,
-    member_casual,
-    COUNT(*) AS total_rides
-FROM trips
-GROUP BY day_of_week, member_casual
-ORDER BY
-    CASE day_of_week
-        WHEN 'Monday' THEN 1
-        WHEN 'Tuesday' THEN 2
-        WHEN 'Wednesday' THEN 3
-        WHEN 'Thursday' THEN 4
-        WHEN 'Friday' THEN 5
-        WHEN 'Saturday' THEN 6
-        WHEN 'Sunday' THEN 7
-    END,
-    member_casual;
+        day_of_week,
+        SUM(CASE WHEN member_casual = 'member' 
+            THEN 1 ELSE 0 END)                          AS member_rides,
+        SUM(CASE WHEN member_casual = 'casual' 
+            THEN 1 ELSE 0 END)                          AS casual_rides,
+        COUNT(*)                                        AS total_rides
+    FROM trips
+    GROUP BY day_of_week
+    ORDER BY 
+        CASE day_of_week
+            WHEN 'Monday'    THEN 1
+            WHEN 'Tuesday'   THEN 2
+            WHEN 'Wednesday' THEN 3
+            WHEN 'Thursday'  THEN 4
+            WHEN 'Friday'    THEN 5
+            WHEN 'Saturday'  THEN 6
+            WHEN 'Sunday'    THEN 7
+        END
