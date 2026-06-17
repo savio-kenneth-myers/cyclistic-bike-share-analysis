6c786d6fd3c8f2291f61ae79a48a0b26848a87c8
savio-kenneth-myers <savio.encrypted@gmail.com>
2026-06-17 20:06:45 +0530

    06_peak_hours.sql

--- SQL/06_peak_hours.sql
+++ SQL/06_peak_hours.sql
@@ -2,9 +2,12 @@
 -- Reveals member commute double peak (8am, 5pm) vs casual single peak (2pm)
 
 SELECT
-    hour,
-    member_casual,
-    COUNT(*) AS total_rides
-FROM trips
-GROUP BY hour, member_casual
-ORDER BY hour, member_casual;
+        hour,
+        SUM(CASE WHEN member_casual = 'member' 
+            THEN 1 ELSE 0 END)                          AS member_rides,
+        SUM(CASE WHEN member_casual = 'casual' 
+            THEN 1 ELSE 0 END)                          AS casual_rides,
+        COUNT(*)                                        AS total_rides
+    FROM trips
+    GROUP BY hour
+    ORDER BY hour ASC
